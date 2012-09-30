036d1de1953935f8b980d67503ea8add81198a9f
Rodrigo Deodoro <roddds@gmail.com>
2012-09-30 16:43:10 -0300

    Now ignored items will be read from a list
    Check ignorelist.txt

--- inv.py
+++ inv.py
@@ -7,12 +7,7 @@ import requests
 import tf2
 import urllib2 #for exception catching
 
-ignored = [u'Mann Co. Supply Crate',            # You can add to this list items that you don't
-           u'Noise Maker - Winter Holiday',     # care about if they're on your inventory or not
-           u'Seal Mask',
-           u'Mann Co. Cap',
-           u'Mercenary',                        # change to a ignorelist file
-           u'Spirit Of Giving']
+ignorelist = [x for x in [line.strip() for line in open('ignorelist.txt', 'r').readlines() if line[0] != '#'] if x]
 
 
 class Backpack:
@@ -93,7 +88,11 @@ class Backpack:
         else:
             bp = backpack+unplaced
 
-        return sorted(['* %s' % hat if hat not in weapons else '  %s' % hat for hat in bp]) #adds an asterisk to the name if it's not a normal weapon
+        bp = [z for z in bp if z not in [x for x in bp for y in ignorelist if y in x]]
+
+        self.bp = bp
+        self.items = sorted(['* %s' % hat if hat not in weapons else '  %s' % hat for hat in bp]) #adds an asterisk to the name if it's not a normal weapon
+        return self.items
 
 
 if __name__ == '__main__':
